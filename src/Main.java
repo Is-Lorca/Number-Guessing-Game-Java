645b2c3f94d0a833282ef319ceb6eeb8d4ecf273
Is-Lorca <isisloliveira@gmail.com>
2026-01-09 14:00:35 -0300

    coding2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,34 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int random_num = random.nextInt(0, 10);
+        int random_num, option, guess;
+
+        //welcome message
+        System.out.println("Welcome to Number Guessing!");
+        System.out.println("Here, we have 3 levels:");
+        System.out.println("Easy - Guess between 0-10;\nMedium - Guess between 0-30;\nHard - Guess between 0-50");
+        System.out.println("Choose the number of the level to start!\n");
+
+        //selecting level
+        System.out.println("1-Easy;\n2-Medium;\n3-Hard.");
+        option = scanner.nextInt();
+
+        switch (option) {
+            case 1 -> {
+
+            }
+            
+            case 2 ->{
+
+            }
+
+            case 3 ->{
+
+            }
+
+            default -> System.out.println("Please select a valid option.");
+                
+        }
+
     }
 }
