ea8f6f2c0e43e8aee0653327b499a4cfc1e80665
Is-Lorca <isisloliveira@gmail.com>
2026-01-09 13:49:30 -0300

    coding

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        //variables
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int random_num = random.nextInt(0, 10);
     }
 }
